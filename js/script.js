4941e40a1e35c2ca05377d57fe214456854fcdd2
Claudia G <claudsain@gmail.com>
2024-05-13 14:11:17 -0400

    Lab finished

--- js/script.js
+++ js/script.js
@@ -2,11 +2,62 @@ window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
 
-  startButton.addEventListener("click", function () {
+  let game
+
+  function startGame() {
+    
+    game = new Game()
+    game.start()
+    
+    console.log("start game");
+    
+    document.addEventListener('keydown', (e) => {
+      
+      if (e.key === "ArrowRight") {
+        if (game.player.directionX < 4) {
+          game.player.directionX += 1
+        }
+      }
+      
+      if (e.key === "ArrowLeft") {
+        if (game.player.directionX > -4) {
+          game.player.directionX -= 1
+        }
+      }
+      
+      if (e.key === "ArrowDown") {
+        if (game.player.directionY < 4) {
+          game.player.directionY += 1
+        }
+      }
+      
+      if (e.key === "ArrowUp") {
+        if (game.player.directionY > -4) {
+          game.player.directionY -= 1
+        }
+      }
+      
+    })
+    
+  }
+
+  function restartGame() {
+
+    startGame()
+
+    game.gameEndScreen.style.display = 'none'
+    game.gameEndScreen.style.padding = 0
+    game.gameEndScreen.style.height = 0
+
+  }
+
+  startButton.addEventListener("click", () => {
     startGame();
   });
 
-  function startGame() {
-    console.log("start game");
-  }
+  restartButton.addEventListener("click", () => {
+    restartGame();
+  });
 };
+
+ 
